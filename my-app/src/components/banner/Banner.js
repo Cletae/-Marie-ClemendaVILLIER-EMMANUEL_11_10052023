5b3f99fc372d8ae2594046b03795ed4b0a6344df
Souvi <souvima@hotmail.com>
2023-07-11 22:58:47 +0200

    Add banner content & img

---
+++ my-app/src/components/banner/Banner.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+import bannerHome from "../assets/images/bannerHome.png";
+
+function Banner() {
+  return (
+    <>
+      <div className="banner-container">
+        <h1>Chez vous, partout et ailleurs</h1>
+        <img src={bannerHome} className="banner-image" alt="home-background" />
+      </div>
+    </>
+  );
+}
+
+export default Banner;
