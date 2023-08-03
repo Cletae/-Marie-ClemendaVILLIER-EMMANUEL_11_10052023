9a4b724499658754c1f135f2227a1e06ed4598fe
Souvi <souvima@hotmail.com>
2023-08-03 18:29:17 +0200

    Add slider img for house

---
+++ my-app/src/components/sliderImage/SliderImage.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./SliderImage.css";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+function SliderImages({ pictures }) {
+  const [current, setCurrent] = useState(0);
+  const length = pictures.length;
+
+  const prev = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  const next = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  return (
+    <div className="sliders">
+      <img src={pictures[current]} className="slide-image" alt="slides" />
+      <FaChevronLeft onClick={prev} className="left-arrow">
+        Prev
+      </FaChevronLeft>
+      <FaChevronRight onClick={next} className="right-arrow">
+        Next
+      </FaChevronRight>
+    </div>
+  );
+}
+
+export default SliderImages;
